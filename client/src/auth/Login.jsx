99ca159c4f9c32654243a1e6a30fb680d9969ede
Racheal <ajasarakel@gmail.com>
2024-09-21 16:59:13 -0700

    first commit

---
+++ client/src/auth/Login.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <>
+    <main className='sign-in-container'>
+    <h2>Login</h2>
+    </main>
+    </>
+  )
+}
+
+export default Login
---
+++ client/src/auth/Signup.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Signup = () => {
+  return (
+    <>
+    <main className='sign-up-container'>
+    <h2>Sign Up</h2>
+    </main>
+    </>
+  )
+}
+
+export default Signup
